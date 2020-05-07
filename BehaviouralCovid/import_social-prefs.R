900c2e2c96fed1c631cf7a86050c2e244a487af2
Orestis Kopsacheilis <orestiskopsacheilis@gmail.com>
2020-05-07 21:59:57 +0100

    nothing much

--- BehaviouralCovid/import_social-prefs.R
+++ BehaviouralCovid/import_social-prefs.R
@@ -1,3 +1,60 @@
-social_prefs<-read.csv("Briq/socialprefs.csv")%>%
-  rename(Country=country)%>%
-  mutate(Country=countrycode(Country,"country.name","country.name"))
+#q<-read.csv("C:/Users/fores/Desktop/COVID19_personal/COVID19-BehPerceptions/GlobalBehaviorsPerceptions_Dataset_May01_2020.csv")
+#saveRDS(q,"GlobalBehCovid.rds")
+
+GlobalBeh<-readRDS("BehaviouralCovid/GlobalBehCovid.rds")%>%
+  rename(Country=CountryofLiving)%>%
+  mutate(Country=countrycode::countrycode(Country,"country.name","country.name"))
+  #mutate_at(vars(beh_stayhome:weight_sample),.funs=as.numeric)
+
+
+g1<-GlobalBeh%>%
+  filter(Country=="United States")%>%
+  mutate(Ap=forcats::fct_relevel(react_pub_appr,
+              "The reaction is not at all sufficient",
+              "The reaction is somewhat insufficient",
+              "The reaction is appropriate",
+              "The reaction is somewhat too extreme",
+              "The reaction is much too extreme"))%>%
+  ggplot(aes(Ap))+
+  geom_bar()+
+  scale_colour_brewer(palette = 'Set1')+
+  theme_classic()+
+  labs(x="",title="Do you think the reaction of your country's public is appropriate,
+       too extreme, or not sufficient?")
+  #ggplot(aes(x=))
+
+
+g2<-GlobalBeh%>%
+  filter(Country=="United States")%>%
+  mutate(Ef=forcats::fct_relevel(perceivedeffectivnes,
+                                 "Not at all effective",
+                                 "Not effective",
+                                 "Neither effective nor ineffective",
+                                 "Effective",
+                                 "Very effective"))%>%
+  ggplot(aes(Ef))+
+  geom_bar()+
+  scale_colour_brewer(palette = 'Set1')+
+  theme_classic()+
+  labs(x="",title="How effective are social distancing
+measures (e.g., through a general curfew) to slow down the spread of the coronavirus?")
+
+
+
+
+g3<-GlobalBeh%>%
+  filter(Country=="United States",govtrust!="6")%>%
+  mutate(Tr=forcats::fct_relevel(govtrust,
+                                 "Strongly distrust",
+                                 "Somewhat distrust",
+                                 "Neither trust nor distrust",
+                                 "Somewhat trust",
+                                 "Strongly trust"))%>%
+  ggplot(aes(Tr))+
+  geom_bar()+
+  scale_colour_brewer(palette = 'Set1')+
+  theme_classic()+
+  labs(x="",title="How much do you trust your country's government to take care of its citizens?")
+
+cowplot::plot_grid(g1,g2,g3,nrow =3)
+ggsave("Trust.png",width=12,height=9)
