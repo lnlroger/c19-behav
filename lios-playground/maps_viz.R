82d5f583f52dc66e7831bb4d229fc1617ececa8d
Lionel Roger <lnl.roger@gmail.com>
2020-07-29 13:44:58 +0200

    Update maps_viz.R
    .

--- lios-playground/maps_viz.R
+++ lios-playground/maps_viz.R
@@ -1,12 +1,16 @@
 register_google(key = "AIzaSyAHf0RUy4W9WiRQDzWG3jVV8UwLUxcmHyY")
-map1 <- get_map(location = "United Kingdom", zoom = 5)
+map1 <- get_map(location = "Magallanes", zoom = 6)
 
 uk <- GpsGoog %>%
   filter(Country.x == "United Kingdom")
 uk.gps_coord <- gps_coord %>%
   filter(Country == "United Kingdom")
 
-ggmap(ggmap = map1) +
-  geom_rect(data=uk, mapping=aes(xmin=west, xmax=east, ymin=south, ymax=north), color="green", alpha=0.5) +
-  geom_rect(data=uk, mapping=aes(xmin=West, xmax=East, ymin=South, ymax=North), color="red", alpha=0.5) +
-  geom_point(data = uk.gps_coord, aes(x=lon,y=lat), colour = "blue")
+charg <- GpsGoog 
+  filter(Country.x == "Chile" | Country.x == "Argentina")
+charg.gps_coord <- gps_coord 
+  filter(Country == "Chile" | Country == "Argentina")
+
+ggmap(ggmap = map1) +  
+  geom_rect(data=charg, mapping=aes(xmin=West, xmax=East, ymin=South, ymax=North), color="red", alpha=0.5) +
+  geom_point(data = charg.gps_coord, aes(x=lon,y=lat), colour = "blue")
