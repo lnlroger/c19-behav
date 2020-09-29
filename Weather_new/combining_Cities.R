f62d4b1afcd02ddcb7616cc63348d1d5ed5bcef5
Orestis Kopsacheilis <orestiskopsacheilis@gmail.com>
2020-09-29 16:35:46 +0200

    intermediate

--- Weather_new/combining_Cities.R
+++ Weather_new/combining_Cities.R
@@ -1,5 +1,5 @@
 #Source: https://www.ncei.noaa.gov/access/search/data-search/global-summary-of-the-day?pageSize=100&dataTypes=%5B%22DEWP%22,%22FRSHTT%22,%22GUST%22,%22MAX%22,%22MIN%22,%22MXSPD%22,%22PRCP%22,%22SLP%22,%22SNDP%22,%22STP%22,%22TEMP%22,%22VISIB%22,%22WDSP%22%5D&bbox=%5B%2239.266,22.500,36.880,25.357%22%5D&startDate=%5B%222019-12-01T00:00:00%22%5D&endDate=%5B%222020-01-01T23:59:59%22%5D
-library("lubridate")
+library(lubridate)
 library(tidyverse)
 library(countrycode)
 library(fuzzyjoin)
@@ -9,7 +9,7 @@ library(ggmap)
 #Load WeatherJan-Jun-2020.rdata
 
 
-source("ImportWeather.r")
+source("Weather_new/ImportWeather.r")
 source("Google/import_mobility.R")
 source("Briq/import_social-prefs.r")
 source("Cities/ImportCities.r")
@@ -298,3 +298,10 @@ gwmp<-gwmp%>%
 
 #FRSHTT is corrupt, along wth the variables derived from it. Leading zeros are dropped somewhere (Excel?); can be fixed if needed
 source("OxfordTracking/covid-policy-tracker-master/data/import_OxCGRT.R")
+
+
+#gwmp2<-left_join(gwmp,Ox,by=c("Country","Date"))
+
+#write_rds(gwmp2,"gwmp.rds")
+
+
